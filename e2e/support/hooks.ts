573081112730f86ca62c783c43c05e4faae7ccde
student <student@safebear.co.uk>
2019-09-18 16:54:19 +0100

    tidying up, still working

--- e2e/support/hooks.ts
+++ e2e/support/hooks.ts
@@ -2,19 +2,11 @@ import { browser } from "protractor";
 
 import { After, Before, Status } from "cucumber";
 
-import { Actions } from "../support/actions";
-import { Checks } from "../support/checks";
-import { Waits } from "../support/waits";
-
 // This will run before each scenario
 Before({timeout: 100 * 1000}, async function() {
 
-  this.actions = new Actions();
-  this.checks = new Checks();
-  this.waits = new Waits();
-
   // Opens the website to the default URL in the 'protractor.config.ts' file
-  await this.actions.openWebsite();
+  await browser.get("");
 
 });
 
--- e2e/support/productsListWorld.ts
+++ e2e/support/productsListWorld.ts
@@ -1,13 +1,7 @@
 import { World } from "cucumber";
-import { Actions } from "./actions";
-import { Checks } from  "./checks";
-import { Waits } from "./waits"; 
 
 declare module "cucumber" {
   interface World {
-    actions: Actions;
-    checks: Checks;
-    waits: Waits;
     product: myLib.Product;
   }
 }
